6889492bbeb67d3115385cd2ddce76bb436bf931
Ken Wakita <wakita@is.titech.ac.jp>
2015-06-23 19:25:24 +0900

    minor change

--- sn/Point.cpp
+++ sn/Point.cpp
@@ -2,12 +2,12 @@
 
 Point::Point(GLfloat r) {
   { // Positions
-    std::unique_ptr<GLfloat[]> _p(new GLfloat[4]);
-    GLfloat *p = _p.get();
+    std::unique_ptr<GLfloat[]> positions(new GLfloat[4]);
+    GLfloat *p = positions.get();
     p[0] = p[1] = p[2] = 0;
     p[3] = r;
     glBindBuffer(GL_ARRAY_BUFFER, vbo[Position]);
-    glBufferData(GL_ARRAY_BUFFER, 4, p, GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, 4, positions.get(), GL_STATIC_DRAW);
     glVertexAttribPointer(Position, 4, GL_FLOAT, GL_FALSE, 0, nullptr);
     glEnableVertexAttribArray(Position);
   }
