4e7fc001b1d568b8a1a7255c695131a55b41a5c1
Ken Wakita <wakita@is.titech.ac.jp>
2015-07-09 21:44:47 +0900

    Removing obsolete files

--- kw/kw1.fs
+++
@@ -1,9 +0,0 @@
-#version 430
-
-layout (location=0) out vec4 Color;
-
-in vec3 color_fs;
-
-void main() {
-  Color = vec4(color_fs, 1);
-}
--- kw/kw2.fs
+++
@@ -1,9 +0,0 @@
-#version 430
-
-layout (location=0) out vec4 FragColor;
-
-in vec4 color_fs;
-
-void main() {
-  FragColor = color_fs;
-}
