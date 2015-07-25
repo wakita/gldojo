d3cfbededa71dd515ef122f1286305a2897267d2
Ken Wakita <wakita@is.titech.ac.jp>
2015-07-25 15:53:18 +0900

    simplification

--- kw/kw7.js
+++ kw/kw7.js
@@ -1,12 +1,6 @@
 #!/usr/bin/env node
 
-var C = require('const');
-
 exports.configure = function (config) {
-  require('cb02a').configure(config);
-  var app = config.app;
   config.trace = true;
-  config.glfw.visible = false;
-  app.shaders = [ 'kw/kw6.shaders' ];
-  config.glfw.title = app.Project = 'kw7';
+  config.glfw.title = config.app.Project = 'kw7: Showing various shader information';
 };
